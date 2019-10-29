12c0f167430c59f88cbadc337b5f697c79718b96
KelC702 <kellamont2@gmail.com>
2019-10-29 17:26:52 -0400

    test

--- Flash/views.py
+++ Flash/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@ login_required
+def index_views(request):
+    user = request.user
+    return render(request, "Flash/index.html", {"user": user})
+
+
--- w8-group-assignment-2-stephen-kelvin-daniel/config/settings.py
+++ w8-group-assignment-2-stephen-kelvin-daniel/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     
     #Third-Parties:
 
+
 ]
 
 MIDDLEWARE = [
@@ -125,8 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
 
 # Authentication
 
